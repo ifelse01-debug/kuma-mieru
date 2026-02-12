4b83ce5301f65192d080d0824d2990aaf2219729
lufei <lufei@lanyang.com>
2026-02-12 16:09:00 +0800

    支持edgeone

--- open-next.config.ts
+++ open-next.config.ts
@@ -1,3 +1,33 @@
-import { defineCloudflareConfig } from '@opennextjs/cloudflare';
-
-export default defineCloudflareConfig();
+export default {
+  default: {},
+  functions: {
+    home: {
+      patterns: ["/"],
+      routes: ["app/page"],
+    },
+    pageId: {
+      patterns: ["/[pageId]"],
+      routes: ["app/[pageId]/page"],
+    },
+    about: {
+      patterns: ["/about"],
+      routes: ["app/about/page"],
+    },
+    apiConfig: {
+      patterns: ["/api/config"],
+      routes: ["app/api/config/route"],
+    },
+    apiHealth: {
+      patterns: ["/api/health"],
+      routes: ["app/api/health/route"],
+    },
+    apiMonitor: {
+      patterns: ["/api/monitor"],
+      routes: ["app/api/monitor/route"],
+    },
+    monitorDetail: {
+      patterns: ["/monitor/[id]"],
+      routes: ["app/monitor/[id]/page"],
+    },
+  },
+};
